2d8f52ecb886430b3efdabe0a836aa9449675e4c
liubz <905808069@qq.com>
2022-08-17 17:31:52 +0800

    1

--- docs/.vuepress/components/Three.vue
+++ docs/.vuepress/components/Three.vue
@@ -3,22 +3,22 @@
 </template>
 
 <script>
-import * as THREE from 'three'
-import { onMounted } from 'vue'
+// import * as THREE from 'three'
+// import { onMounted } from 'vue'
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-)
+// const scene = new THREE.Scene()
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// )
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-onMounted(() => {
-  document.body.appendChild(renderer.domElement)
-})
+// const renderer = new THREE.WebGLRenderer()
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// onMounted(() => {
+//   document.body.appendChild(renderer.domElement)
+// })
 </script>
 
 <style scoped></style>
